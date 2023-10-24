591f8b9ef804fb2a0e9d8c0e45f7289df23b26b7
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-24 16:31:43 +0200

    Rocop correction

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,8 +1,8 @@
 class GreetingsController < ApplicationController
-    def index
-        @random_greeting = Greeting.order('RANDOM()').first
-        respond_to do |format|
-         format.render json: { greeting: @random_greeting.message }
-        end
+  def index
+    @random_greeting = Greeting.order('RANDOM()').first
+    respond_to do |format|
+      format.render json: { greeting: @random_greeting.message }
     end
+  end
 end
